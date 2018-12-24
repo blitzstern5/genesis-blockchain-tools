104687a795bb17c5a3d4b9a6b461d0141b0a0ae8
blitzstern5 <blitzstern5@gmail.com>
2018-12-24 08:14:26 +0300

    Rename functions wallet_to_adddres to address_to_key_id, address_to_wallet to key_id_to_address

--- genesis_blockchain_tools/convert/genesis.py
+++ genesis_blockchain_tools/convert/genesis.py
@@ -3,17 +3,16 @@ import ctypes
 from . import fill_from_left
 
 ADDRESS_LENGTH = 20
+ADDRESS_PART_LEN = 4
+ADDRESS_PART_DELIM = '-'
 
-def address_to_wallet(address, use_delims=True):
-    s = str(int(address) & 0xffffffffffffffff)
+def key_id_to_address(key_id, use_delims=True):
+    s = str(ctypes.c_uint64(key_id).value)
     s = fill_from_left(s, exp_len=ADDRESS_LENGTH)
     if use_delims:
-        s = '-'.join((s[0:4], s[4:8], s[8:12], s[12:16], s[16:20]))
+        s = ADDRESS_PART_DELIM.join([s[i: i + ADDRESS_PART_LEN] for i in range(0, len(s), ADDRESS_PART_LEN)])
     return s
 
-key_id_to_wallet = address_to_wallet
+def address_to_key_id(address):
+    return ctypes.c_int64(int(address.replace(ADDRESS_PART_DELIM, '').lstrip('0'))).value
 
-def wallet_to_address(wallet):
-    return ctypes.c_int64(int(wallet.replace('-', '').lstrip('0'))).value
-
-wallet_to_key_id = wallet_to_address
--- tests/test_convert_genesis.py
+++ tests/test_convert_genesis.py
@@ -1,12 +1,11 @@
 import pytest
 
 from genesis_blockchain_tools.convert.genesis import (
-    address_to_wallet, wallet_to_address,
-    key_id_to_wallet, wallet_to_key_id
+    key_id_to_address, address_to_key_id
 )
 
-def test_address_to_wallet():
-    fns = (address_to_wallet, key_id_to_wallet)
+def test_key_id_to_address():
+    fns = (key_id_to_address,)
     for i in range(0, len(fns)):
         assert fns[0](-7129004159526005831) == '1131-7739-9141-8354-5785'
         assert fns[0](-7129004159526005831, use_delims=False) == '11317739914183545785'
@@ -14,8 +13,8 @@ def test_address_to_wallet():
         assert fns[0](7822345485898532948) == '0782-2345-4858-9853-2948'
 
 
-def test_wallet_to_address():
-    fns = (wallet_to_address, wallet_to_key_id)
+def test_address_to_key_id():
+    fns = (address_to_key_id,)
     for i in range(0, len(fns)):
         assert fns[0]('1131-7739-9141-8354-5785') == -7129004159526005831
         assert fns[0]('11317739914183545785') == -7129004159526005831
