e811bf80a0dc768f3df4245e59ff4ccda5e267f1
blitzstern5 <blitzstern5@gmail.com>
2019-07-16 02:28:42 +0300

    Add more tests based on fixture data from tests/fixture/data.json

--- tests/test_convert_genesis.py
+++ tests/test_convert_genesis.py
@@ -4,6 +4,12 @@ from genesis_blockchain_tools.convert.genesis import (
     key_id_to_address, address_to_key_id
 )
 
+from .utils import load_fixture_data
+
+
+FIXTURE_DATA = load_fixture_data()
+
+
 def test_key_id_to_address():
     fns = (key_id_to_address,)
     for i in range(0, len(fns)):
@@ -11,6 +17,11 @@ def test_key_id_to_address():
         assert fns[0](-7129004159526005831, use_delims=False) == '11317739914183545785'
         assert fns[0](3496019374267252370) == '0349-6019-3742-6725-2370'
         assert fns[0](7822345485898532948) == '0782-2345-4858-9853-2948'
+
+    for rec in FIXTURE_DATA:
+        key_id = rec[0]
+        address = rec[1]
+        assert fns[0](key_id) == address
 
 
 def test_address_to_key_id():
@@ -22,4 +33,9 @@ def test_address_to_key_id():
         assert fns[0]('0782-2345-4858-9853-2948') == 7822345485898532948
         assert fns[0]('07822345485898532948') == 7822345485898532948
         assert fns[0]('7822345485898532948') == 7822345485898532948
+
+    for rec in FIXTURE_DATA:
+        key_id = rec[0]
+        address = rec[1]
+        assert fns[0](address) == key_id
 
--- tests/test_crypto_genesis.py
+++ tests/test_crypto_genesis.py
@@ -1,5 +1,9 @@
 import pytest
 
+from genesis_blockchain_tools.convert.genesis import (
+    key_id_to_address, address_to_key_id
+)
+
 from genesis_blockchain_tools.crypto import (
     gen_private_key, get_public_key, gen_keypair,
 )
@@ -8,6 +12,12 @@ from genesis_blockchain_tools.crypto.genesis import (
     checksum, public_key_to_address, public_key_to_key_id, double_hash
 )
 
+from .utils import load_fixture_data
+
+
+FIXTURE_DATA = load_fixture_data()
+
+
 def test_checksum():
     assert checksum("some string".encode()) == 9
     assert checksum("another string".encode()) == 2
@@ -15,15 +25,27 @@ def test_checksum():
     assert checksum("and final one".encode()) == 7
     assert checksum("#34423".encode()) == 1
 
-def test_public_key_to_address():
+def test_public_key_to_key_id():
     priv_key = gen_private_key()
-    fns = (public_key_to_address, public_key_to_key_id)
+    fns = (public_key_to_key_id,)
     for i in range(0, len(fns)):
         assert fns[0]("some string".encode()) == 6973694608268913209
         assert fns[0]("another string".encode()) == 826786831444702840
         assert fns[0]("jeez, it's so boring".encode()) == -1106672694297211255
         assert fns[0]("and final one".encode()) == -5961772402273559210
         assert fns[0]("#34423".encode()) == 6729523148133476469 
+
+    for rec in FIXTURE_DATA:
+        key_id = rec[0]
+        public_key = rec[2]
+        assert fns[0](bytes.fromhex(public_key)) == key_id
+
+def test_public_key_address():
+    fns = (public_key_to_address,)
+    for rec in FIXTURE_DATA:
+        address = rec[1]
+        public_key = rec[2]
+        assert fns[0](bytes.fromhex(public_key)) == address
 
 def test_double_hash():
     assert double_hash("some string".encode()) == bytearray([190, 71, 146, 194, 68, 74, 231, 230, 180, 206, 236, 27, 236, 231, 72, 142, 255, 142, 193, 16, 191, 23, 17, 134, 130, 233, 238, 189, 116, 61, 77, 16])
