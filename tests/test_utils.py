ac2fa7103ddef522a0b0537d86f12ffdffd544d7
blitzstern5 <blitzstern5@gmail.com>
2019-07-15 03:26:45 +0300

    Add support of an object with mime_type attribute searching. Add more test cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,34 @@
 import pytest
 import re
+from collections import namedtuple
 
 from genesis_blockchain_tools.utils import find_mime_type_recursive
 
 def test_find_mime_type_recursive():
     d = [['.gif', 'image/gif', 'Graphics interchange format file (GIF87a)', 0.4], ['.gif', '', 'GIF file', 0.4]]
     assert find_mime_type_recursive(d) == 'image/gif'
+
+    d = [['.gif', 'image/gif', 'Graphics interchange format file (GIF87a)', 0.4], ['.gif', '', 'GIF file', 0.4]]
+    assert find_mime_type_recursive(d) != 'image/png'
+
+    d = {
+        'byte_match': 'value1',
+        'offset': '123',
+        'mime_type': 'image/gif',
+        'extension': '.gif',
+        'name': 'gif name',
+    } 
+    NamedTuple = namedtuple('NamedTuple', tuple(d.keys()))
+    nt = NamedTuple(**d)
+    assert find_mime_type_recursive(nt) == 'image/gif'
+
+    d = {
+        'byte_match': 'value1',
+        'offset': '123',
+        'mime_type': 'image/png',
+        'extension': '.png',
+        'name': 'png name',
+    } 
+    NamedTuple = namedtuple('NamedTuple', tuple(d.keys()))
+    nt = NamedTuple(**d)
+    assert find_mime_type_recursive(nt) != 'image/gif'
