e338c723175a84e655b4ea0f027cdbc795b5a664
blitzstern5 <blitzstern5@gmail.com>
2019-07-16 02:27:43 +0300

    Add function to load fixture data from tests/fixture/data.json

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,10 @@
 import string
 import random
+import json
+import os
 
 def gen_rand_str(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-
 
 def get_rand_pub_key_fmt():
     fmts = (None, 'RAW', '04')
@@ -12,3 +13,9 @@ def get_rand_pub_key_fmt():
 def get_rand_sign_fmt():
     fmts = (None, 'RAW', 'DER')
     return random.choice(fmts)
+
+def load_fixture_data():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'fixture', 'data.json')
+    with open(path) as f:
+        return json.load(f)
